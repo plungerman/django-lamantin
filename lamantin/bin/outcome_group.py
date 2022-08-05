926790242b30255ce742a1e80773e78407d223f6
plungerman <plungerman@gmail.com>
2022-08-05 16:22:19 -0500

    starting permissions infrastructure

--- lamantin/bin/outcome_group.py
+++ lamantin/bin/outcome_group.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import django
 import os
 import sys
 
-import django
 django.setup()
+
+from django.conf import settings
+from django.contrib.auth.models import User
 from lamantin.geoc.models import Course
 from lamantin.geoc.models import Outcome
 
@@ -17,6 +20,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lamantin.settings.shell')
 def main():
     """Main function that does something."""
     course = Course.objects.get(pk=3)
+    user = User.objects.get(pk=settings.FACULTY_EXPLORATIONS)
+    print(user)
     for outcome in course.outcome.all():
         print(outcome.group)
 
