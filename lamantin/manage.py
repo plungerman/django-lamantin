3a555de5c20e50138a41bc3f16592f2ab46e1302
plungerman <plungerman@gmail.com>
2024-02-02 12:47:53 -0600

    updated python path

--- lamantin/manage.py
+++ lamantin/manage.py
@@ -4,7 +4,7 @@ import sys
 
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djlamantin.settings.shell')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lamantin.settings.shell')
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
